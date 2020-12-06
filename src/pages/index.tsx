1d6d07f5647d51cdad2a1dab40b47fc5be03f5f7
frrfdev <feliperenanrf23@gmail.com>
2020-12-05 21:32:27 -0300

    register working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
-const Index = () => <h1>Hello</h1>;
+import { useEffect } from 'react';
+import { useMeQuery } from '../generated/graphql';
+
+const Index = () => {
+	const [me, runMeQuery] = useMeQuery();
+	useEffect(() => {
+		runMeQuery();
+	}, []);
+
+	return (
+		<div>
+			<h1>Hello {me.data?.me?.username}</h1>
+		</div>
+	);
+};
 
 export default Index;
